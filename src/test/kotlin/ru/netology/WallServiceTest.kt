8809a8aceb4e6aed47e90cdd504f61e6cf976681
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-30 20:26:46 +0300

    Устраниние ошибки

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,16 +1,11 @@
 package ru.netology
 
 import org.junit.Assert.*
-import org.junit.After
-import org.junit.Before
+
 import org.junit.Test
 
 class WallServiceTest {
 
-    @Before
-    fun clearBeforeTest() {
-        WallService.clear()
-    }
 
     @Test
     fun update_ForTrue() {
